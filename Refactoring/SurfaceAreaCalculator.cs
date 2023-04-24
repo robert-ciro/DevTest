da7ef4c24cf941f553718b7cdf964b6be24004f2
robertparra.ciro <robertparra.ciro@alpinagroup.com>
2023-04-24 21:10:34 +0200

    🔨 refactor: Removed duplicated code

--- Refactoring/SurfaceAreaCalculator.cs
+++ Refactoring/SurfaceAreaCalculator.cs
@@ -113,15 +113,7 @@ namespace Refactoring
                 default:
                     ShowCommands:
                     this.logger.Log("Unknown command!!!");
-                    this.logger.Log("commands:");
-                    this.logger.Log("- create square {double} (create a new square)");
-                    this.logger.Log("- create circle {double} (create a new circle)");
-                    this.logger.Log("- create rectangle {height} {width} (create a new rectangle)");
-                    this.logger.Log("- create triangle {height} {width} (create a new triangle)");
-                    this.logger.Log("- print (print the calculated surface areas)");
-                    this.logger.Log("- calculate (calulate the surface areas of the created shapes)");
-                    this.logger.Log("- reset (reset)");
-                    this.logger.Log("- exit (exit the loop)");
+                    this.ShowCommands();
                     this.ReadString(Console.ReadLine());
                     break;
             }
