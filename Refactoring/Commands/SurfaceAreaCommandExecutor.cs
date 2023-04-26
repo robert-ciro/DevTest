7b6213909ce2462f9b68610586496858356d1b39
robertparra.ciro <robertparra.ciro@alpinagroup.com>
2023-04-26 14:22:42 +0200

    🔨 refactor: Moved ShowCommands into their own class

--- Refactoring/Commands/SurfaceAreaCommandExecutor.cs
+++ Refactoring/Commands/SurfaceAreaCommandExecutor.cs
@@ -68,7 +68,7 @@ namespace Refactoring.Commands
                     }
                     else
                     {
-                        ShowCommands();
+                        new HelpCommand(logger).Execute();
                     }
                     this.ExecuteCommand(userInterface.ReadMessage());
                     break;
@@ -99,29 +99,16 @@ namespace Refactoring.Commands
                 case "exit":
                     break;
                 case "show":
-                    ShowCommands();
+                    new HelpCommand(logger).Execute();
                     ExecuteCommand(userInterface.ReadMessage());
                     break;
                 default:
                     ShowCommands:
                     this.logger.Log("Unknown command!!!");
-                    this.ShowCommands();
+                    new HelpCommand(logger).Execute();
                     ExecuteCommand(userInterface.ReadMessage());
                     break;
             }
-        }
-
-        private void ShowCommands()
-        {
-            this.logger.Log("commands:");
-            this.logger.Log("- create square {double} (create a new square)");
-            this.logger.Log("- create circle {double} (create a new circle)");
-            this.logger.Log("- create rectangle {height} {width} (create a new rectangle)");
-            this.logger.Log("- create triangle {height} {width} (create a new triangle)");
-            this.logger.Log("- print (print the calculated surface areas)");
-            this.logger.Log("- calculate (calulate the surface areas of the created shapes)");
-            this.logger.Log("- reset (reset)");
-            this.logger.Log("- exit (exit the loop)");
         }
     }
 }
